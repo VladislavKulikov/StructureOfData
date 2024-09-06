c2dc1ff81ec82a82f2fe9d70a2ba2d179c192459
Denvill11 <127538626+Denvill11@users.noreply.github.com>
2024-09-06 14:27:53 +0300

    Update hashTable.js

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -1,7 +1,7 @@
 class HashTable {
   constructor(size) {
     this.size = size;
-    this.table = new Array(size);
+    this.table = new Array(size).fill(null).map(() => []);
   }
 
   hash(key) {
@@ -13,38 +13,59 @@ class HashTable {
   }
 
   set(key, value) {
-    let index = this.hash(key);
-    if (!this.table[index]) {
-      this.table[index] = [];
+    const index = this.hash(key);
+    const bucket = this.table[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        return;
+      }
     }
-    this.table[index].push([key, value]);
-    return this.table;
+
+    bucket.push([key, value]);
   }
 
   get(key) {
-    let index = this.hash(key);
+    const index = this.hash(key);
     const bucket = this.table[index];
-    if (bucket) {
-      for (let i = 0; i < bucket.length; i++) {
-        if (bucket[i][0] === key) {
-          return bucket[i][1];
-        }
+
+    for (let i = 0; i < bucket.length; i++) {
+      console.log(1);
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
       }
     }
+
     return undefined;
   }
 
   remove(key) {
-    let index = this.hash(key);
+    const index = this.hash(key);
     const bucket = this.table[index];
-    if (bucket) {
-      for (let i = 0; i < bucket.length; i++) {
-        if (bucket[i][0] === key) {
-          bucket.splice(i, 1);
-          return 
-        }
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        return;
       }
     }
+
     return undefined;
   }
+
+  find(callback) {
+    for (let i = 0; i < this.size; i++) {
+      const bucket = this.table[i];
+      for (let j = 0; j < bucket.length; j++) {
+        const [key, value] = bucket[j];
+        if (callback(value, key)) return value;
+      }
+    }
+  }
 }
+
+const table = new HashTable(2);
+table.set("1123123123", 1);
+table.set("asdasdas", 2);
+console.log(table.get("asdasdas"));
